048cb8c01c76e75f3ae7928af8a75251e47f3728
facelessKV <facelesszp@gmail.com>
2025-04-01 00:01:09 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,8 @@
+# Файл конфигурации для хранения API ключей
+# Замените на ваши реальные значения
+
+# Токен бота Telegram
+BOT_TOKEN = "YOUR_TELEGRAM_BOT_TOKEN"
+
+# API ключ OpenWeatherMap
+OWM_API_KEY = "YOUR_OPENWEATHERMAP_API_KEY"
